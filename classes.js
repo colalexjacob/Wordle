c68c1ec9bebae11623e5fd6765ef14f4534347bf
colalexjacob <colalexjacob@gmail.com>
2022-06-27 22:46:35 +0530

    A Wordle clone
    This is a clone of the NYT Wordle game.
    I created this to practice HTML / CSS and JavaScript.
    The words list was picked up from https://www.wordunscrambler.net/word-list/wordle-word-list
    This contains 2309 words
    
    I had initially used the word list from https://github.com/tabatkins/wordle-list/blob/main/words which has 12947 words, but I found that there were many, many non-English words (Scottish?, Welsh?, Irish? Made-up?, therefore I was forced to change the words list.

--- classes.js
+++ classes.js
@@ -1,103 +1,296 @@
+"use strict";
+
+/*******************
+ * *****************
+ * @name DisplayTile
+ * @description One tile
+ *
+ * *****************
+ ******************/
+
 class DisplayTile {
-    static keyChangedEvent = new Event("keyChanged", {"bubbles":true, "cancelable":true});
+  #key = null;
+  get key() {
+    return this.#key;
+  }
+  set key(value) {
+    if (value === "") {
+      value = null;
+    }
+    if (value >= 97 && value <= 122) {
+      value -= 32;
+    }
+    if (value >= 65 && value <= 90) {
+      this.#key = value;
+      this.#displayTile.innerText = String.fromCharCode(value);
+    } else if (value == null) {
+      this.#key = value;
+      this.#displayTile.innerText = "";
+    }
+  }
 
-    #key = null;
-    get key() {
-        return this.#key;
-    }
-    set key(value) {
-        if(value === "") {
-            value = null;
-        }
-        if(value >= 65 && value <= 90) {
-            if(value != this.#key){
-                this.#key = value;
-                this.dispatchEvent('function eventHandler(event) {');
-            }
-        }
-        else if (value == null) {
-            if(value != this.#key){
-                this.#key = value;
-                this.dispatchEvent('keyChangedEvent');
-            } 
-        }
-    }
+  toString() {
+    return this.#key ? String.fromCharCode(this.#key) : null;
+  }
 
-    toString() {
-        return this.#key ? String.fromCharCode(this.#key) : null;
-    }
+  get hasValue() {
+    return !(this.#key == null);
+  }
 
-    hasValue() {
-        return !(this.#key == null);
-    }
+  #wasHintTaken = false;
+  get wasHintTaken() {
+    return this.#wasHintTaken;
+  }
+  set wasHintTaken(value) {
+    this.#wasHintTaken = value ? true : false;
+  }
 
-    #wasHintTaken = false;
-    get wasHintTaken() {
-        return this.#wasHintTaken;
-    }
-    set wasHintTaken (value) {
-        this.#wasHintTaken = value ? true : false;
-    }
+  #displayTile = null;
+  get displayTile() {
+    return this.#displayTile;
+  }
+  set displayTile(value) {
+    let str = value.tagName;
 
-    #displayTile = null;
-    get displayTile() {
-        return this.#displayTile;
+    if (str === "DIV" && value.classList.contains("tile")) {
+      this.#displayTile = value;
     }
-    set displayTile(value) {
-       this.#displayTile = value; 
+  }
+
+  #tileStatus = TileStatus.blank;
+  get tileStatus() {
+    return this.#tileStatus;
+  }
+  set tileStatus(value) {
+    switch (value) {
+      case TileStatus.blank:
+        this.#tileStatus = TileStatus.blank;
+        this.displayTile.classList.remove(
+          "darkGrayTile",
+          "yellowTile",
+          "greenTile"
+        );
+        this.displayTile.classList.add("lightGrayTile");
+        break;
+      case TileStatus.wrong:
+        this.#tileStatus = TileStatus.wrong;
+        this.displayTile.classList.remove(
+          "lightGrayTile",
+          "yellowTile",
+          "greenTile"
+        );
+        this.displayTile.classList.add("darkGrayTile");
+        break;
+      case TileStatus.yellow:
+        this.#tileStatus = TileStatus.yellow;
+        this.displayTile.classList.remove(
+          "lightGrayTile",
+          "greenTile",
+          "darkGrayTile"
+        );
+        this.displayTile.classList.add("yellowTile");
+        break;
+      case TileStatus.green:
+        this.#tileStatus = TileStatus.green;
+        this.displayTile.classList.remove(
+          "lightGrayTile",
+          "yellowTile",
+          "darkGrayTile"
+        );
+        this.displayTile.classList.add("greenTile");
+        break;
+      default:
+        this.#tileStatus = TileStatus.blank;
+        this.displayTile.classList.remove(
+          "darkGrayTile",
+          "yellowTile",
+          "greenTile"
+        );
+        this.displayTile.classList.add("lightGrayTile");
+        break;
     }
-
-    addEventListener(type, callback){
-        
-    }
-
-    eventHandler(event) {
-        if(event.type === 'keyChanged') {
-
-        }
-    }
-
+  }
 }
 
+/*******************
+ * *****************
+ * @name DisplayRow
+ * @description One row of tiles
+ *
+ * *****************
+ ******************/
+
 class DisplayRow {
-    constructor() {
-        this.tiles = [];
+  constructor() {
+    this.tiles = []; //Holds DisplayTiles
+  }
+
+  tilePointer = new currentTileTracker(
+    this.onBeforeTileChanged,
+    this.onAfterTileChanged
+  );
+
+  addTile(value) {
+    let str = value.tagName;
+    if (this.tiles.length < 5) {
+      this.tiles.push(value);
     }
+  }
 
-    static tileAddedEvent = new Event("tileAdded", {"bubbles":true, "cancelable":true});
+  #documentRow = null;
+  get documentRow() {
+    return this.#documentRow;
+  }
+  set documentRow(value) {
+    let str = value.tagName;
+    if (str === "DIV" && value.classList.contains("wodrleRow")) {
+      this.#documentRow = value;
+    }
+  }
 
-    addTile(value) {
-        if(this.tiles.length < 5){
-            this.tiles.push(value);
-            this.#documentRow.dispatchEvent('tileAdded');
+  areAllTilesFilled() {
+    for (let i = 0; i < this.tiles.length; i++) {
+      const tile = this.tiles[i];
+      if (!tile.hasValue) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  getWord() {
+    if (this.areAllTilesFilled()) {
+      let str = "";
+      for (let i = 0; i < this.tiles.length; i++) {
+        const elt = this.tiles[i];
+        str += elt.toString();
+      }
+      return str;
+    } else {
+      return null;
+    }
+  }
+
+  get currentTileIndex() {
+    return this.tilePointer.currentTileIndex;
+  }
+  set currentTileIndex(value) {
+    this.tilePointer.currentTileIndex = value;
+  }
+
+  get currentTile() {
+    return this.tiles[this.currentTileIndex];
+  }
+
+  onBeforeTileChanged() {
+    let dr = rows[currentRow];
+    let dt = dr.tiles[dr.currentTileIndex].displayTile;
+    dt.classList.remove("tileFocusWithin");
+    dt.blur();
+  }
+
+  onAfterTileChanged() {
+    let dr = rows[currentRow];
+    let dt = dr.tiles[dr.currentTileIndex].displayTile;
+    dt.classList.add("tileFocusWithin");
+    dt.focus();
+  }
+
+  getTileIndex(displayTile) {
+    if (
+      displayTile.tagName === "DIV" &&
+      displayTile.classList.contains("tile")
+    ) {
+      for (let i = 0; i < this.tiles.length; i++) {
+        if (displayTile === this.tiles[i].displayTile) {
+          return i;
         }
+      }
     }
+    return -1;
+  }
+}
 
-    #documentRow = null;
-    get documentRow() {
-        return this.#documentRow;
-    }
-    set documentRow(value) {
-       this.#documentRow = value; 
-    }
+class currentTileTracker {
+  #noOfTiles = TILES_PER_ROW;
+  get noOfTiles() {
+    return this.#noOfTiles;
+  }
 
+  #callbackBeforeCurrentTileChanged;
+  get callbackBeforeCurrentTileChanged() {
+    return this.#callbackBeforeCurrentTileChanged;
+  }
+  set callbackBeforeCurrentTileChanged(value) {
+    if (value === null || typeof value == "function") {
+      this.#callbackBeforeCurrentTileChanged = value;
+    }
+  }
+
+  #callbackAfterCurrentTileChanged;
+  get callbackAfterCurrentTileChanged() {
+    return this.#callbackAfterCurrentTileChanged;
+  }
+  set callbackAfterCurrentTileChanged(value) {
+    if (value === null || typeof value == "function") {
+      this.#callbackAfterCurrentTileChanged = value;
+    }
+  }
+
+  constructor(fn_beforeCurrentTileChanged, fn_afterCurrentTileChanged) {
+    this.callbackBeforeCurrentTileChanged = fn_beforeCurrentTileChanged;
+    this.callbackAfterCurrentTileChanged = fn_afterCurrentTileChanged;
+  }
+
+  #currentTileIndex = 0;
+  get currentTileIndex() {
+    return this.#currentTileIndex;
+  }
+  set currentTileIndex(value) {
+    if (value != this.#currentTileIndex) {
+      if (value >= 0 && value < this.#noOfTiles) {
+        this.callbackBeforeCurrentTileChanged?.();
+        this.#currentTileIndex = value;
+        this.callbackAfterCurrentTileChanged?.();
+      }
+    }
+  }
+
+  moveNext() {
+    if (this.#currentTileIndex < this.#noOfTiles - 1) {
+      this.currentTileIndex += 1;
+    }
+  }
+
+  movePrevious() {
+    if (this.#currentTileIndex > 0) {
+      this.currentTileIndex -= 1;
+    }
+  }
 }
 
 class Words {
-    constructor() {
-        this.wordsList = [];
-    }
+  constructor() {
+    this.wordsList = [];
+  }
 
-    loadWords() {
-        this.wordsList = words_List;
-    }
+  loadWords() {
+    this.wordsList = words_List;
+  }
 
-    contains(word) {
-        return wordsList.contains(word);
-    }
+  contains(word) {
+    return wordsList.contains(word);
+  }
 
-    getRandomWord() {
-        let index = Math.floor(Math.random() * this.wordsList.length);
-        return this.wordsList[index];
-    }
+  getRandomWord() {
+    let index = Math.floor(Math.random() * this.wordsList.length);
+    return this.wordsList[index];
+  }
+}
+
+class TileStatus {
+  static blank = Symbol("blank");
+  static wrong = Symbol("wrong");
+  static yellow = Symbol("yellow");
+  static green = Symbol("green");
 }
--- script.js
+++ script.js
@@ -1,76 +1,232 @@
+"use strict";
+
 const NO_OF_ROWS = 6;
 const TILES_PER_ROW = 5;
+const DICT_URL = "https://www.dictionary.com/browse/";
 
 let words; // Class. Contains list of words
-let rowsContainer; // DOM element to hold all rows 
-let rows = [];
+let rowsContainer; // DOM element to hold all rows
+let rows = []; // Contains 06 x rows of 05 x tiles each
+let keyBoardkeys; // Nodelist of all keyboard keys
+
+let btnNewGame; // Starts a new game
+let btnGiveUp; // Discards current game
+let btnHint; // Reveals letter of current tile
+let btnMeaning; // Checks meaning of current word in dictionary.com
+
+let wordToGuess = null;
+let isGameOver = false;
+let isGameWon = false;
 
 let currentRow = 0;
-let currentCol = 0;
 
-window.addEventListener('load', onWindowLoad);
+window.addEventListener("load", onWindowLoad);
 
 function onWindowLoad(event) {
-    words = new Words();
-    words.loadWords();
+  words = new Words();
+  words.loadWords();
+  wordToGuess = words.getRandomWord();
 
-    rowsContainer = document.querySelector('#rowsContainer');
-    setUpTiles();
+  btnNewGame = document.querySelector("#newGame");
+  btnNewGame.addEventListener("click", onBtnNewGameClick);
+  btnGiveUp = document.querySelector("#giveUp");
+  btnGiveUp.addEventListener("click", onBtnGiveUpClick);
+  btnHint = document.querySelector("#hint");
+  btnHint.addEventListener("click", onBtnHintClick);
+  btnMeaning = document.querySelector("#meaning");
+  btnMeaning.addEventListener("click", onBtnMeaningClick);
 
-    let keyBoardkeys = document.querySelectorAll(".keyBoardRow .keyBoardKey" );
-    for (let index = 0; index < keyBoardkeys.length; index++) {
-        keyBoardkeys[index].addEventListener('click', onKeyBoardKeyClick);
-    }
+  rowsContainer = document.querySelector("#rowsContainer");
+  setUpTiles();
+
+  keyBoardkeys = document.querySelectorAll(".keyBoardRow .keyBoardKey");
+  for (let index = 0; index < keyBoardkeys.length; index++) {
+    keyBoardkeys[index].addEventListener("click", onKeyBoardKeyClick);
+  }
+
+  document.addEventListener("keydown", onKeyDown);
+
+  rows[currentRow].tilePointer.callbackAfterCurrentTileChanged?.();
 }
 
 function setUpTiles() {
-    if(rows.length > 0) {
-        rows.length = 0;
+  if (rows.length > 0) {
+    rows.length = 0;
+  }
+
+  for (let index = 0; index < NO_OF_ROWS; index++) {
+    let newRow = document.createElement("div");
+    newRow.id = `row${index}`;
+    newRow.className = "wodrleRow";
+    let dr = new DisplayRow();
+    dr.documentRow = newRow;
+    rows.push(dr);
+
+    for (let n = 0; n < TILES_PER_ROW; n++) {
+      let newTile = document.createElement("div");
+      newTile.id = `R${index}C${n}`;
+      newTile.tabIndex = 0;
+      newTile.className = "tile lightGrayTile";
+      newTile.addEventListener("click", onTileClick);
+      let dt = new DisplayTile();
+      dt.displayTile = newTile;
+      dr.tiles.push(dt);
+      newRow.append(newTile);
     }
+    rowsContainer.append(newRow);
+  }
+}
 
-    for (let index = 0; index < NO_OF_ROWS; index++) {
-        let newRow = document.createElement("div");
-        newRow.id = `row${index}`;
-        newRow.className = 'wodrleRow';
-        let dr = new DisplayRow();
-        dr.documentRow = newRow;
-        rows.push(dr);
+function onKeyDown(event) {
+  let chrCode = event.key.charCodeAt(0);
+  if (event.key === "Backspace") {
+    chrCode = 8;
+  } else if (event.key === "Enter") {
+    chrCode = 13;
+  } else if (chrCode >= 65 && chrCode <= 90) {
+    chrCode += 32;
+  }
 
-        for (let n = 0; n < TILES_PER_ROW; n++) {
-            let newTile = document.createElement("div");
-            newTile.id = `R${index}C${n}`;
-            newTile.className = 'tile lightGrayTile';
-            let dt = new DisplayTile;
-            dt.displayTile = newTile;
-            dt.addEventListener('keyChanged', onTileKeyChanged);
-            dr.tiles.push(dt);
-            newRow.append(newTile);
-        }
-        rowsContainer.append(newRow);
-    }   
+  if (chrCode == 8 || chrCode == 13 || (chrCode >= 97 && chrCode <= 122)) {
+    doKeyPress(chrCode);
+  }
+}
+
+function onTileClick(event) {
+  if (isGameOver || isGameWon) {
+    return;
+  }
+  let tileClicked = event.target;
+  let dr = rows[currentRow];
+  let tileIndex = dr.getTileIndex(tileClicked);
+
+  if (tileIndex >= 0) {
+    dr.currentTileIndex = tileIndex;
+  }
 }
 
 function onKeyBoardKeyClick(event) {
-    let keyBoardKey = event.target;
-    let chrCode = Number(keyBoardKey.dataset.key);
+  let keyBoardKey = event.target;
+  let chrCode = Number(keyBoardKey.dataset.key);
 
-    doKeyPress(chrCode);
+  doKeyPress(chrCode);
 }
 
 function doKeyPress(key) {
-    if(key === 8) {
-        if(currentCol > 0){
+  if (isGameOver || isGameWon) {
+    return;
+  }
 
-        }
+  let dr = rows[currentRow];
+  if (key === 8) {
+    if (dr?.currentTileIndex >= 0) {
+      let dt = dr.tiles[dr.currentTileIndex];
+      if (dt.hasValue) {
+        dt.key = null;
+        dr.tilePointer.movePrevious();
+      } else {
+        dr.tilePointer.movePrevious();
+        dt = dr.tiles[dr.currentTileIndex];
+        dt.key = null;
+      }
     }
-    if(currentCol < 5){
-        if(key === 8 || key === 13 || (key >= 97 && key <= 122)){
-
-        }
+  } else if (key === 13) {
+    if (dr?.areAllTilesFilled()) {
+      checkWord(dr);
+      if (isGameOver || isGameWon) {
+        return;
+      } else if (currentRow < NO_OF_ROWS) {
+        dr?.onBeforeTileChanged();
+        currentRow++;
+        rows[currentRow]?.tilePointer?.callbackAfterCurrentTileChanged?.();
+      }
+      if (currentRow == NO_OF_ROWS) {
+        alert(`You lose. The word was: ${wordToGuess}`);
+        isGameOver = true;
+      }
     }
+  } else if (dr?.currentTileIndex < TILES_PER_ROW && key >= 97 && key <= 122) {
+    dr.currentTile.key = key - 32;
+    dr.tilePointer.moveNext();
+  }
 }
 
 function onTileKeyChanged(event) {
-    let tile = event.target;
-    tile.displayTile.innerText = tile.key;
+  let tile = event.target;
+  tile.displayTile.innerText = tile.key;
+}
+
+function checkWord(dr) {
+  let wds = [...wordToGuess];
+
+  if (dr.getWord() === wordToGuess) {
+    isGameOver = true;
+    for (let i = 0; i < dr.tiles.length; i++) {
+      dr.tiles[i].tileStatus = TileStatus.green;
+      setKeyboardKeyColour(dr.tiles[i].toString(), "greenTile");
+      dr.tiles[i].displayTile.classList.add("flip");
+    }
+  } else {
+    for (let i = 0; i < dr.tiles.length; i++) {
+      const elt = dr.tiles[i];
+      if (wds[i] === elt.toString()) {
+        elt.tileStatus = TileStatus.green;
+        setKeyboardKeyColour(dr.tiles[i].toString(), "greenTile");
+      } else if (wds.includes(elt.toString())) {
+        elt.tileStatus = TileStatus.yellow;
+        setKeyboardKeyColour(dr.tiles[i].toString(), "yellowTile");
+      } else {
+        elt.tileStatus = TileStatus.wrong;
+        setKeyboardKeyColour(dr.tiles[i].toString(), "darkGrayTile");
+      }
+    }
+  }
+}
+
+function setKeyboardKeyColour(value, classToAdd) {
+  let key = value.charCodeAt(0);
+  if (key >= 65 && key <= 90) {
+    key += 32;
+  }
+  if (key < 97 || key > 122) {
+    return;
+  }
+
+  let str = `.keyBoardKey[data-key="${key}"]`;
+  let keybdKey = document.querySelector(str);
+
+  keybdKey?.classList.add(classToAdd);
+}
+
+function onBtnNewGameClick() {
+  if (isGameOver || isGameWon) {
+    location.reload();
+  }
+}
+
+function onBtnGiveUpClick() {
+  if (isGameOver || isGameWon) {
+    return;
+  }
+  if (confirm("Are you sure you want to give up?")) {
+    alert(`You lose. The word was: ${wordToGuess}`);
+    isGameOver = true;
+  }
+}
+
+function onBtnHintClick() {
+  let dr = rows[currentRow];
+  let tileIndex = dr.currentTileIndex;
+
+  dr.currentTile.key = wordToGuess.charCodeAt(tileIndex);
+  dr.currentTile.displayTile.classList.add("hintTaken");
+}
+
+function onBtnMeaningClick() {
+  let dr = rows[currentRow];
+  let str = dr.getWord();
+  if (str) {
+    str = DICT_URL + str;
+    window.open(str, "_blank");
+  }
 }
